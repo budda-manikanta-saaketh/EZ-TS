a6227c08d8625145520101dae304b37678fd544f
budda-manikanta-saaketh <115549218+budda-manikanta-saaketh@users.noreply.github.com>
2023-09-28 14:29:46 +0530

    commit

---
+++ day-2/linear_search.py
@@ -0,0 +1,14 @@
+def find(li,num):
+    f=0
+    for i in li:
+        if num==i:
+            print ("number found")
+            f=1
+            break
+    return f
+li=list(map(int,input("enter the list elements:").split(" ")))
+num=int(input("enter the number to search:"))
+f=find(li,num)
+if f==0:
+    print(f"the element {num} is not present ")
+        
