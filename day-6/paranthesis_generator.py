ff31953bf9cbdb8a724149b112d0351feea80111
budda-manikanta-saaketh <115549218+budda-manikanta-saaketh@users.noreply.github.com>
2023-10-05 14:29:01 +0530

    day-6

--- day-6/paranthesis_generator.py
+++ day-6/paranthesis_generator.py
@@ -1,3 +1,23 @@
+def balanced(s):
+    stack=[]
+    for i in s:
+        if i=="(" or i=="{" or i=="[":
+            stack.append(i)
+        elif i==")" or i=="}" or i=="]":
+            if len(stack)==0:
+                return False
+            elif i==")" and stack[-1]=="(":
+                stack.pop()
+            elif i=="}" and stack[-1]=="{":
+                stack.pop()
+            elif i=="]" and stack[-1]=="[":
+                stack.pop()
+            else:
+                return False
+    if len(stack)==0:
+        return True
+    else:
+        return False
 def gen_par(n):
     res=[]
     def par(n,l,r,s):
@@ -5,8 +25,17 @@ def gen_par(n):
             par(n,l+1,r,s+"(")
         if r<l:
             par(n,l,r+1,s+")")
+        if l<n:
+            par(n,l+1,r,s+"{")
+        if r<l:
+            par(n,l,r+1,s+"}")
+        if l<n:
+            par(n,l+1,r,s+"[")
+        if r<l:
+            par(n,l,r+1,s+"]")
         if l==n and r==n:
-            res.append(s)
+            if balanced(s):
+                res.append(s)
     par(n,0,0,"")
     return res
 
